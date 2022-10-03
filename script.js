ebd9ed66cd45dc191a950f62d54ae63136dc1459
Danil <danilkuznecov15@gmail.com>
2022-10-03 16:02:06 +0500

    HW-JavaScript5

--- script.js
+++ script.js
@@ -5,37 +5,24 @@ console.log('');
 
 const users = {}
 
-for (let i = 1; i <= 3; i++ ) {    
+for (let i = 1; i <= 10; i++ ) {    
     let a = prompt('Введите имя')    
     let b = +prompt('Введите возраст');      
 
 
-    users[`Пользователь: - ${i}`] = {
-        '1': {
-            name: `${a}`,
-            age: `${b}`            
-        }        
+    users[i] = {        
+        name: a,
+        age: b   
+               
     };    
     
 };
 
 for(let key in users) {
-    console.log(key);
+    console.log(`Пользователь: - ${key}`);
     for(let newKey in users[key]) {
-        console.log(users[key][newKey]);
+        console.log(newKey == 'name' ? `Ваше имя ${users[key][newKey]}` : `Ваш возраст ${users[key][newKey]}`);
     }
 }
 
 console.log(users);
-
-
-
-
-
-
-
-
-
-
-
-    
